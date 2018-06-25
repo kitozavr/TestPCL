b12a2321bd8a0e89df455d384d1ea212086afc3b
kitozavr <36747282+kitozavr@users.noreply.github.com>
2018-06-25 21:56:44 +0300

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+
+using System.Net;
+
+namespace TestPCL
+{
+	public partial class MainPage : ContentPage
+	{
+        WebClient client = new WebClient();
+        //-----------------------
+        //Тестовый комментарий 1
+        //-----------------------
+		public MainPage()
+		{
+			InitializeComponent();
+            //client.DownloadFile("test.com", "test_file");
+            label1.Text = client.ToString();
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            button1.Text = client.ToString();
+        }
+
+    }
+}
